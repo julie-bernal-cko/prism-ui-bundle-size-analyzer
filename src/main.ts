6c5dde981028fcbf997b921fbb3b0ee769a506c1
Julie Bernal <julie.bernal@checkout.com>
2021-01-28 15:39:10 +0000

    removed unused code and clean up file

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,10 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const directory: string = core.getInput('directory')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('directory', directory)
   } catch (error) {
     core.setFailed(error.message)
   }
